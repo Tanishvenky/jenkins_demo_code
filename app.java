c80f93a29e34fbe1b784131765246c8fda783686
Tanishvenky <venkateshdevkate09@gmail.com>
2024-11-24 16:18:46 +0530

    removed dot

--- app.java
+++ app.java
@@ -2,7 +2,7 @@ publc class App {
 
     public static void main (String[] args){
         System.out.println("Hello Jenkins");
-        System.out.println("second line...");
+        System.out.println("second line..");
     }
     
 }
